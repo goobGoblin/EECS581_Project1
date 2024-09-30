906f0b32a182e80126e9c070716a4e2b891926d7
melton-jason <meltonjason12@yahoo.com>
2024-09-29 23:15:12 -0500

    Fix having more than one positional arg

--- easy.py
+++ easy.py
@@ -2,7 +2,7 @@ from ai_player import BattleshipAI
 import random
 
 class EasyAI(BattleshipAI):
-    def make_move(self):
+    def make_move(self, hit=False):
         print(self.shots)
         while True:
             row = random.randint(0, 9)
